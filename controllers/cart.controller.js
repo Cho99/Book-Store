56ecf565790834919c71b814e40eca8f6b8b058a
Glitch (book-store-final-lesson) <none>
2020-05-08 14:24:50 +0000

    😎🛣 Updated with Glitch

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,6 +1,20 @@
 const Cart = require("../models/cart.model");
 
-module.export.cartAdd = (req, res) => {
+module.exports.cartAdd = async (req, res) => {
   const bookId = req.params.bookId;
+  const sessionId = req.signedCookies.sessionId;
   
+  if(!sessionId) {
+    res.redirect("/");
+    return;
+  } 
+  
+  const count = await Cart.findOne({userId: sessionId});
+  console.log( JSON.parse(count.cart));
+  res.send("Hello")
+  
+}
+
+module.exports.index = async (req, res) => {
+  res.send("Hello")
 }
